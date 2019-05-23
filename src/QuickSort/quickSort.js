d91b47169bc20829461391ea51c69ba6eb0f3218
My420 <my420V@gmail.com>
2019-05-23 15:46:06 +0300

    refactoring quickSort Alghoritm

--- src/QuickSort/quickSort.js
+++ src/QuickSort/quickSort.js
@@ -2,18 +2,14 @@ const quickSort = function quickSortAlghoritm(list) {
   if (list.length < 2) {
     return list;
   }
-  const baseElement = list.splice(0, 1);
-  const smallerElements = list.filter(elem => {
-    return elem <= baseElement[0];
-  });
+  const baseElement = list[0];
+  const smallerElements = list.slice(1).filter(elem => elem <= baseElement);
 
-  const biggerElements = list.filter(elem => {
-    return elem > baseElement[0];
-  });
+  const biggerElements = list.slice(1).filter(elem => elem > baseElement);
 
   return [
     ...quickSortAlghoritm(smallerElements),
-    ...baseElement,
+    baseElement,
     ...quickSortAlghoritm(biggerElements)
   ];
 };
